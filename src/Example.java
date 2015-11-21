4b4372dc5451ad42ddfead37c22a66880d9ffeb6
Milosz <milosz.maniuk@gmail.com>
2015-11-21 15:26:55 +0100

    Changed Main and Example

--- src/Example.java
+++ src/Example.java
@@ -1,4 +1,23 @@
-
+/**
+* INFO: This is class
+*/
 public class Example {
+	/**
+	 * INFO: This is Field
+	 */
+	private int myIntegerField;
+	
+	/**
+	 * INFO: This is method
+	 */
+	public void display() {
+		System.out.println("My field: " + myIntegerField);
+	}
 
+	/**
+	 * INFO; This is metod (constructor)
+	 */
+	public Example() {
+			myIntegerField = 0;
+	}
 }
